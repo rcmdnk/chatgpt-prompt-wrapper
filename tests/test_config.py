d2a989c0e5dc477a7c0d79f94b0603eeec242b67
rcmdnk <rcmdnk@gmail.com>
2023-09-23 11:27:20 +0900

    chore: Bump version to 0.0.15 in pyproject.toml chore: Add conf-finder package to treat configurations.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,7 @@
-from chatgpt_prompt_wrapper.config import get_config
-
 try:
     import tomllib
 except ModuleNotFoundError:
     import tomli as tomllib
-
-
-def test_get_config(conf_file):
-    assert conf_file == get_config("cg", "config.toml")
 
 
 def test_example_config(conf_file):
