811b84b6f6bd5f588752f52903e19eda630b424a
rcmdnk <rcmdnk@gmail.com>
2026-03-05 10:03:41 +0900

    chore: update pre-commit hooks, dev dependencies, and project classifiers refactor: improve version retrieval robustness and test utilities test: add git tag validation test

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,14 +1,30 @@
+import importlib
 from pathlib import Path
 
-try:
-    import tomllib
-except ModuleNotFoundError:
-    import tomli as tomllib
+import pytest
+from git import Repo
+from git.exc import InvalidGitRepositoryError
 
 from chatgpt_prompt_wrapper import __version__
 
 
-def test_version():
-    with open(Path(__file__).parents[1] / "pyproject.toml", "rb") as f:
+def test_version() -> None:
+    try:
+        tomllib = importlib.import_module("tomllib")
+    except ModuleNotFoundError:
+        tomllib = importlib.import_module("tomli")
+
+    with (Path(__file__).parents[1] / "pyproject.toml").open("rb") as f:
         version = tomllib.load(f)["project"]["version"]
     assert version == __version__
+
+
+def test_tag() -> None:
+    try:
+        repo = Repo(Path(__file__).parents[1])
+    except InvalidGitRepositoryError:
+        pytest.skip("Not a git repo.")
+    tags = repo.git.tag(sort="creatordate").splitlines()
+    if len(tags) > 0:
+        latest_tag = tags[-1]
+        assert latest_tag == "v" + __version__
